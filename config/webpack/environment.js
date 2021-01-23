75f89a5a4a2c087ef473057ce16d9505dff8b268
junjun491 <bestofjunjun@gmail.com>
2021-01-23 14:09:32 +0900

    bootstrap_webpackers_install

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// ここから
+// jQueryとBootstapのJSを使えるように
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+// ここまで
+
 
 module.exports = environment
