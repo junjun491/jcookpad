9f9ac9217b58f1ab15ba6fc10c670c45b4d7d0f1
junjun491 <bestofjunjun@gmail.com>
2021-03-22 00:04:02 +0900

    mod search

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
   mount_uploader :rimage, ImageUploader #追記
 
-  def self.search(search)
+  def self.search(rn, ing)
     return Post.all unless search
-    Post.where(['rname LIKE ?', "%#{search}%"])
+    Post.where(['rname LIKE ?', "%#{rn}%"]).Post.where(['ingredient LIKE ?', "%#{ing}%"])
   end
 
 end
