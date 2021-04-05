54b20c5988d756e7bf1df5dfba868c9562517dfa
junjun491 <bestofjunjun@gmail.com>
2021-04-06 08:30:07 +0900

    fix application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require Chart.min　追加
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -10,7 +12,6 @@ import "channels"
 import 'bootstrap';
 import '../stylesheets/application';
 
-//= require Chart.min　追加
 
 Rails.start()
 Turbolinks.start()
