c26c4f23470ce22b50ca986b876f53f9458378a4
junjun491 <bestofjunjun@gmail.com>
2021-05-19 21:59:33 +0900

    format sitaka

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+          es6: true,
+            },
+              extends: [
+                  // 省略,
+                      "prettier",
+                        ],
+};
