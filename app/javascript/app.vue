0eb8dd4dc0b4128cd9e765f9970c5dc1a2180fdd
junjun491 <bestofjunjun@gmail.com>
2021-05-29 21:58:45 +0900

    mod edit page2

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -10,6 +10,7 @@ import VueRouter from 'vue-router'
 import PostEditPage from './packs/home/PostEditPage.vue'
 import PostNewPage from './packs/home/PostNewPage.vue'
 const router = new VueRouter({
+  
   routes: [
     { path: '/',
       component: PostNewPage  },
@@ -20,6 +21,7 @@ const router = new VueRouter({
 // ref. https://jp.vuejs.org/v2/guide/plugins.html#%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%81%AE%E4%BD%BF%E7%94%A8
 Vue.use(VueRouter)
 export default {
+  
   router
 }
 </script>
