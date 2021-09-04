f701120a193dbfa08f39d92c4645da009383e136
root <root@ip-10-0-10-145.ap-northeast-1.compute.internal>
2021-09-04 04:12:27 +0000

    commit all

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ CSV.foreach('db/seed_stds2.csv') do |row|
   Standard.create(Category: row[0], xEnergy: row[1], xProtein: row[2], xLipid: row[3], xCarbohydrate: row[4], xDietary_fiber: row[5], xPotassium: row[6], xCalcium: row[7], xiron: row[8], xZinc: row[9], xVitaminA: row[10], xVitaminB1: row[11], xVitaminB2: row[12], xVitaminC: row[13], xSalt_equivalent: row[14])
 end
 
-CSV.foreach('db/ingredients_rev08.csv') do |row|
-  Post.create(rname: row[0], Energy: row[1], Protein: row[2], Lipid: row[3], Carbohydrate: row[4], Dietary_fiber: row[5], Potassium: row[6], Calcium: row[7], iron: row[8], Zinc: row[9], VitaminA: row[10], VitaminB1: row[11], VitaminB2: row[12], VitaminC: row[13], Salt_equivalent: row[14], rinformation: row[15])
-end
+#CSV.foreach('db/ingredients_rev08.csv') do |row|
+#  Post.create(rname: row[0], Energy: row[1], Protein: row[2], Lipid: row[3], Carbohydrate: row[4], Dietary_fiber: row[5], Potassium: row[6], Calcium: row[7], iron: row[8], Zinc: row[9], VitaminA: row[10], VitaminB1: row[11], VitaminB2: row[12], VitaminC: row[13], Salt_equivalent: row[14], rinformation: row[15])
+#end
