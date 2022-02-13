bb15518e659a608534ce79d7ec53aca02307c309
junjun491 <bestofjunjun@gmail.com>
2022-02-13 20:16:53 +0900

    change2

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import Vue from "vue";
-import VueRouter from "vue-router";
-import PostEditPage from "./packs/home/PostEditPage.vue";
-import PostNewPage from "./packs/home/PostNewPage.vue";
-import LikeListPage from "./packs/like/LikeListPage.vue";
-const router = new VueRouter({
-  routes: [
-    { path: "/", component: PostNewPage },
-    { path: "/:id(\\d+)/edit", component: PostEditPage },
-    { path: "/user/:id(\\d+)/favorite", component: LikeListPage },
-  ],
-});
-// ref. https://jp.vuejs.org/v2/guide/plugins.html#%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%81%AE%E4%BD%BF%E7%94%A8
-Vue.use(VueRouter);
-export default {
-  router,
-};
+
+ export default {
+-  router,
+-};
++  data: function () {
++    return {
++      message: "Hello Vue!"
++    }
++  }
++}
 </script>
 
+-
 <style></style>
++
+<style scoped>
++p {
++  font-size: 2em;
++  text-align: center;
++}
++
+</style>
