e67277e53095af1140d7f83ca9bf9886b5a7ddbe
junjun491 <bestofjunjun@gmail.com>
2022-12-24 15:30:07 +0900

    add password param at users.rb and create apipost file

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     sequence(:email, "test1@gmail.com")
     encrypted_password              {"12345678"}
     age {"30"}
-    sex {"1"}
+    sex {"man"}
+    password {"123123"}
     sequence(:name, "name1")
   end
 end
---
+++ spec/requests/api_v1_post_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+ RSpec.describe 'ApiV1Post', type: :request do
+  before do
+    @post= FactoryBot.create(:post)
+    get api_v1_post_path(@post.id)
+  end
+
+ it 'statusが200になること' do
+    json = JSON.parse(response.body)
+    expect(response.status).to eq(200)
+ end
+end
