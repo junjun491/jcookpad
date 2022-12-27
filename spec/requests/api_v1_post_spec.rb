3bda3ccf3af395aff719adacc56cdf4b572e37a9
junjun491 <bestofjunjun@gmail.com>
2022-12-27 21:03:22 +0900

    add password param at users.rb and create apipost file_rev02

--- spec/requests/api_v1_post_spec.rb
+++ spec/requests/api_v1_post_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
  RSpec.describe 'ApiV1Post', type: :request do
-  before do
-    @post= FactoryBot.create(:post)
-    get api_v1_post_path(@post.id)
-  end
-
+  let(:post) { create(:post) }
+  before { get api_v1_post_path(post.id) }
  it 'statusが200になること' do
     json = JSON.parse(response.body)
     expect(response.status).to eq(200)
+    expect(json['id']).to eq(post.id)
  end
 end
