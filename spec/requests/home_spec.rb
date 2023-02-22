6281dac6d9d8d9ccbaeb6c8d355d470ec8c56a6d
junjun491 <bestofjunjun@gmail.com>
2023-02-22 22:19:20 +0900

    rspec対応20230222

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
-require '/eiyorecipe/spec/support/utilities.rb'
-require '/eiyorecipe/spec/shared/login.rb'
+require 'support/utilities'
+require 'shared/login'
+
 
 RSpec.describe "Home", type: :request do
+  before do
 
-  describe 'GET #index' do
-    let!(:user) { create(:user) }
-    context 'ログインしているとき' do
-      include_context 'login_as_user'
-
-        it '年齢と性別に応じて適切な栄養摂取基準を取得できること' do
-         expect(user.get_standard).to eq Standard.find_by(id: 9) 
-        end
-    end
-      context 'パラメータが不正なとき' do
-
-      end
-    context 'ログインしていないとき' do
-      it 'メッセージが投稿できないこと' do
-
-      end
-    end
   end
+    describe 'GET #index' do
+
+      context 'ログインしているとき' do
+        include_context 'login_as_user'
+  
+          it 'ログインユーザの年齢と性別に応じて適切な栄養摂取基準を取得できること' do
+           expect(login_user.get_standard).to eq Standard.find_by(id: 9) 
+           get posts_path
+           expect(response).to have_http_status 200
+          end
+      end
+
+      context 'ログインしていないとき' do
+        it '問題なくhttp通信できること' do
+          get posts_path
+          expect(response).to have_http_status 200
+         end
+
+
+      end
+    end
 end
