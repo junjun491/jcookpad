76290e41075ebc2221973b90e571b77b11fb49bc
junjun491 <bestofjunjun@gmail.com>
2023-02-23 13:31:18 +0900

    rspec周り修正20230223

--- spec/requests/api_v1_post_spec.rb
+++ spec/requests/api_v1_post_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
- RSpec.describe 'ApiV1Post', type: :request do
-  let(:post) { create(:post) }
-  before { get api_v1_post_path(post.id) }
- it 'statusが200になること' do
-    json = JSON.parse(response.body)
-    expect(response.status).to eq(200)
-    expect(json['id']).to eq(post.id)
- end
+RSpec.describe 'ApiV1Post', type: :request do
+   describe 'ApiV1Post#show' do
+      let(:post) { create(:post) }
+      before { get api_v1_post_path(post.id) }
+
+      it 'idでgetリクエストしたpost(レシピ)の中身も問題なくリクエストしたものであること(代表で名前のみチェック)' do
+         json = JSON.parse(response.body)
+         expect(json['rname']).to eq(post.rname)
+      end
+
+      it 'httpのリクエストが成功し,statusが200のレスポンスがかえってくること' do
+         expect(response.status).to eq(200)
+      end
+   end
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
-require 'support/utilities'
-require 'shared/login'
-
 
 RSpec.describe "Home", type: :request do
-  before do
 
-  end
     describe 'GET #index' do
 
+      before do
+        get posts_path
+      end
+
       context 'ログインしているとき' do
-        include_context 'login_as_user'
-  
-          it 'ログインユーザの年齢と性別に応じて適切な栄養摂取基準を取得できること' do
-           expect(login_user.get_standard).to eq Standard.find_by(id: 9) 
-           get posts_path
-           expect(response).to have_http_status 200
-          end
+        let(:login_user) { create :user }
+        before { sign_in(login_user) }
+
+        it 'ログインユーザの年齢と性別に応じて適切な栄養摂取基準を取得できること' do
+          expect(login_user.get_standard).to eq Standard.find_by(id: 9) 
+        end
+
+        it '問題なくhttp通信ができること' do
+          expect(response).to have_http_status 200
+        end
+
       end
 
       context 'ログインしていないとき' do
         it '問題なくhttp通信できること' do
-          get posts_path
           expect(response).to have_http_status 200
-         end
-
-
+        end
       end
     end
 end
